8d4b9fe793d8adf6fe19ec1d1a1419846573cb60
Tooba1166 <163329886+Tooba1166@users.noreply.github.com>
2024-06-02 19:34:32 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+alert ("Answer the following question");
+let score = 0;
+let totalScore = 5;
+let ques1 = 'Can we use CamelCase in JS?';
+let ans1 = "Yes";
+let ques2 =  "Shall underscore use as variable?";
+let ans2 = "Yes";
+let ques3 =  "Does JS doent follow BODMAS rule";
+let ans3 = "No";
+let ques4 =  "Do this language sound complex to you?";
+let ans4 = "Yes";
+let ques5 = "Is it integrated with CSS?"
+let ans5 = "No";
+
+
+let response1 = prompt(ques1);
+let response2 = prompt(ques2);
+let response3 = prompt(ques3);
+let response4 = prompt(ques4);
+let response5 = prompt(ques5);
+
+if (response1 === ans1) {
+    score++
+}
+
+if (response2 === ans2) {
+    score++
+}
+
+if (response3 === ans3) {
+    score++
+}
+
+if (response4 === ans4) {
+    score++
+}
+
+if (response5 === ans5) {
+    score++
+}
+
+alert (score/totalScore*100)
